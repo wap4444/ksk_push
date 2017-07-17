1bf8e1ffd92110c6c865fe11aad1946bc04837e2
wap4444 <wap4444@mail.ru>
2017-07-17 17:30:09 +0600

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -55,10 +55,11 @@ var rrPush=0;
     }
 
 function didOpenRemoteNotificationCallBack(jsonData) {
-alert('Было убито');
+
 rr=1; rrPush=1;
 // Для Andori
 var newdata = JSON.parse ( jsonData.notification.payload.additionalData );
+    alert(newdata.ssylka+' rrPush='+rrPush);
 var ref = cordova.InAppBrowser.open(newdata.ssylka , '_blank', 'location=no,toolbar=no,disallowoverscroll=yes');
  //  Для Iphone
  //  var ref = cordova.InAppBrowser.open(jsonData.notification.payload.additionalData.ssylka, '_blank', 'location=no,toolbar=no,disallowoverscroll=yes');

89e7f735c466b2ee391a26d6af39dfda09f7258b
wap4444 <wap4444@mail.ru>
2017-07-22 20:28:41 +0600

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -29,6 +29,7 @@ var app = {
     // Application Constructor
     initialize: function() {
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+	document.addEventListener("offline", onOffline, false);
     },
 
     // deviceready Event Handler
@@ -38,31 +39,15 @@ var app = {
     onDeviceReady: function() {
         this.receivedEvent('deviceready');
     },
+function onOffline() {
+alert('NO DATA');
+}
+
 
     
     // Update DOM on a Received Event
-    receivedEvent: function(id) {
-	    
-	    function checkConnection() {
-    var networkState = navigator.connection.type;
- 
-    var states = {};
-    states[Connection.UNKNOWN]  = 'Unknown connection';
-    states[Connection.ETHERNET] = 'Ethernet connection';
-    states[Connection.WIFI]     = 'WiFi connection';
-    states[Connection.CELL_2G]  = 'Cell 2G connection';
-    states[Connection.CELL_3G]  = 'Cell 3G connection';
-    states[Connection.CELL_4G]  = 'Cell 4G connection';
-    states[Connection.CELL]     = 'Cell generic connection';
-    states[Connection.NONE]     = 'No network connection';
- 
-    alert('Connection type: ' + states[networkState]);
-}
- 
-checkConnection();
-	    
-	    
-var rr = 0;
+    receivedEvent: function(id) {  
+	var rr = 0;
         var iosSettings = {};
         iosSettings["kOSSettingsKeyAutoPrompt"] = false;
         iosSettings["kOSSettingsKeyInAppLaunchURL"] = true;
